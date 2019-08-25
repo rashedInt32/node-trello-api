389631a4702771297ea46ac0adbfd1e413167b1b
rashedInt32 <parvez08eee@gmail.com>
2019-08-25 17:13:34 +0600

    protected routes middleware'

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+import { config } from '../config/config';
+
+const auth = (req, res, next) => {
+  const token = req.header('x-auth-token');
+  if (!token)
+    return res.status(401).send({
+      error: true,
+      msg: 'Access denied, not token provided'
+    })
+
+  try {
+    const decode = jwt.verify(token, config.jwtPrivateKey);
+    req.user = decode;
+    next();
+  } catch (ex) {
+    req.status(400).send({error: true, msg: 'Invalid Token'})
+  }
+}
+
+export default auth;
