6bc76b65bc17fae146e27b5632cdaee9ec1e2b57
rashedInt32 <parvez08eee@gmail.com>
2019-09-10 00:24:35 +0600

    Creates list schema

---
+++ src/models/listSchema.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import Joi from 'joi';
+
+const listSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  idMemberCreator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  idBoard: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Board'
+  },
+  closed: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const List = mongoose.model('list', listSchema);
+
+const validateList = list => {
+  const listSchema = {
+    name: Joi.string().required()
+  };
+
+  return Joi.validate(list, listSchema);
+};
+
+
+export { List, valideList };
