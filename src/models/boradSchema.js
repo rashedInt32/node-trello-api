e05b2c4b0e377f41fbe557038a49e43053efdacc
rashedInt32 <parvez08eee@gmail.com>
2019-09-10 00:05:15 +0600

    Added board schema

---
+++ src/models/boradSchema.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+import Joi from 'joi';
+
+const boardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 100
+  },
+  closed: {
+    type: Boolean,
+    default: false
+  },
+  invited: {
+    type: Boolean,
+    default: false
+  },
+  membership: [{
+    idMember: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    memberType: String,
+  }],
+});
+
+const Board = mongoose.model('board', boardSchema);
+
+const validateBoard = (board) => {
+  const boardSchema = {
+    name: Joi.string().min(5).max(100).required()
+  }
+
+  return Joi.validate(board, boardSchema);
+};
+
+export { Board, validateBoard };
