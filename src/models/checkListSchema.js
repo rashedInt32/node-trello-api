77f1f4431989b7b564fb51215556074e6f56d488
rashedInt32 <parvez08eee@gmail.com>
2019-09-10 21:31:53 +0600

    Creates checlist schema

--- src/models/checkListSchema.js
+++ src/models/checkListSchema.js
@@ -1,4 +1,28 @@
+import mongoose from 'mongoose';
 
 const checkListSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  idBoard: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Board"
+  },
+  idCard: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Card"
+  },
+  checkItems: [
+    {
+      state: String,
+      idCheckList: mongoose.Schema.Types.ObjectId,
+      ref: 'CheckList',
+      name: String
+    }
+  ]
+});
 
-})
+const CheckList = mongoose.model('checklist', checkListSchema);
+
+export { CheckList };
