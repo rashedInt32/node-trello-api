e68e74f0a6f38711b4d963ea0697d27eb32ca627
rashedInt32 <parvez08eee@gmail.com>
2019-09-17 22:59:44 +0600

    Checklist items creates api

--- src/routes/api/checklist.js
+++ src/routes/api/checklist.js
@@ -16,4 +16,32 @@ router.post('/create', Auth, async (req, res) => {
   res.status(200).send(checklist);
 });
 
+
+/**
+ * @routes POST /api/checklist/:id/checkitem
+ * @desc Create check list items
+ * @api private
+ */
+router.post('/:id/checkitem', auth, async (req, res) => {
+  const { id } = req.params.id;
+  const { name, idCheckList } = req.body;
+
+  let checkList = await CheckList.findById(id);
+  if (!checkList)
+    return res.status(400).send({
+      error: true,
+      msg: 'No checklist found'
+    });
+
+  checkList.checkItems.push({
+    status: 'open',
+    name,
+    idCheckList
+  });
+
+  checkList.save();
+
+  res.status(200).send(checkList);
+});
+
 export default router;
