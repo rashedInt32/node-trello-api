a71b5f1b02a70817fc20fd67863a6b6e8e8cb375
rashedInt32 <parvez08eee@gmail.com>
2020-03-21 19:57:12 +0600

    chang config

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const PORT = process.env.PORT
 // const MONGODB_URI = process.env.MONGODB_URI
 // const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
 const MONGODB_URI =
-  "mongodb://parvez210:parvez081118@ds213079.mlab.com:13079/trello";
+  "mongodb://rashedInt32:parvez081118@ds213079.mlab.com:13079/trello";
 const JWT_SECRET_KEY = "mysecretkey";
 
 export {
