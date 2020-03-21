e48dfe2beabed7e8e7ce9bc9b1d7c516cdc41d9e
rashedInt32 <parvez08eee@gmail.com>
2020-03-21 18:15:56 +0600

    Delete build folder

--- build/config/config.example.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var config = exports.config = {
-  dbUri: "mongodb://localhost/[DB-Name]"
-};
-//# sourceMappingURL=config.example.js.map
